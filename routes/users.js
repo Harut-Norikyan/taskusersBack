a95d57d2c27d978759925cad2bc849a0c81059e4
Harut-Norikyan <harutnoro@mail.ru>
2020-09-16 18:21:31 +0400

    upl

--- routes/users.js
+++ routes/users.js
@@ -17,28 +17,6 @@
 
 
 
-// const express = require("express");
-// const router = express.Router();
-// const multer  = require("multer");
-// const upload = require("../middlewares/fileUpload")
-
-// router.use(express.static(__dirname));
-
-// router.use(multer({dest:"uploads"}).single("photo"));
-// router.post("/add-user", function (req, res, next) {
-
-//     let filedata = req.body.photo;
-//     console.log(filedata);
-
-//     if(!filedata)
-//         res.send("Ошибка при загрузке файла");
-//     else
-//         res.send("Файл загружен");
-// });
-// module.exports = router
-
-
-
 
 var express = require('express')
 var multer  = require('multer')
@@ -49,6 +27,9 @@ const router = express.Router();
 router.post('/add-user', upload.single('photo'), function (req, res, next) {
     console.log(req.file,"req.file");
     console.log(req.body,"req.body");
+    res.send({
+        status:true
+    })
 })
 module.exports = router
 
